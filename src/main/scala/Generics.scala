8182333a4d563b5a5ef2335aaaf9eeb200476088
Johannes Coetzee <johannes@jemstep.com>
2019-04-12 09:38:33 +0200

    Adding some generics

--- src/main/scala/Generics.scala
+++ src/main/scala/Generics.scala
@@ -2,4 +2,12 @@ package main.scala
 
 class Generics {
 
+  def getMonoidForMyThing[T](whatever : T) : Monoid[T] = new Monoid[T] {
+    override def retrieve: T = whatever
+  }
+
+}
+
+trait Monoid[A] {
+  def retrieve : A
 }
