e5aa2e95742dc769ac7f9254afe7235c4cbcd00d
Johannes Coetzee <johannes@jemstep.com>
2019-04-16 08:32:41 +0200

    Removing list and using seq as we have no dependency on list

--- src/main/scala/Mapper.scala
+++ src/main/scala/Mapper.scala
@@ -2,22 +2,22 @@ package main.scala
 
 object Mapper {
 
-  def intMap[T](mapFunc: Int => T, list: List[Int]): List[T] = {
+  def intMap[T](mapFunc: Int => T, Seq: Seq[Int]): Seq[T] = {
     for {
-      item <- list
+      item <- Seq
     } yield mapFunc(item)
   }
 
-  def genMap[Input, Return](mapFunc: Input => Return)(implicit list: List[Input]): List[Return] = {
+  def genMap[Input, Return](mapFunc: Input => Return)(implicit Seq: Seq[Input]): Seq[Return] = {
     for {
-      item <- list
+      item <- Seq
     } yield mapFunc(item)
   }
 
-  implicit class MyMapper[Input](list: List[Input]) {
-    def genMap[Return](mapFunc: Input => Return): List[Return] = {
+  implicit class MyMapper[Input](Seq: Seq[Input]) {
+    def genMap[Return](mapFunc: Input => Return): Seq[Return] = {
       for {
-        item <- list
+        item <- Seq
       } yield mapFunc(item)
     }
   }
